eb934db2b366a34221c8ea0d767069ae71cf3351
y9matoDv <bim653267@gmail.com>
2024-06-14 12:58:53 +0300

    Update Solution.java

--- Palindrome Number/Solution.java
+++ Palindrome Number/Solution.java
@@ -1,1 +1,13 @@
+class Solution {
+  public boolean isPalindrome(int x) {
+    String string = String.valueOf(x):
+    int length = str.length();
 
+    for (int i = 0; i < length / 2; i++) {
+      if (string.charAt(i) != string.charAt(length - i - 1)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
