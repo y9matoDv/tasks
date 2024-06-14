a4b93d1760c13af52f90318a8dcb156478ae9e88
y9matoDv <bim653267@gmail.com>
2024-06-14 13:00:23 +0300

    Update Solution.java

--- Palindrome Number/Solution.java
+++ Palindrome Number/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
   public boolean isPalindrome(int x) {
     String string = String.valueOf(x):
-    int length = str.length();
+    int length = string.length();
 
     for (int i = 0; i < length / 2; i++) {
       if (string.charAt(i) != string.charAt(length - i - 1)) {
